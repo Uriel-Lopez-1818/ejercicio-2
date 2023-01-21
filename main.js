c7e69a7c98505ab6d7a498271c98ec3d64738417
Uriel-Lopez-1818 <uriellopezpacheco7805@gmail.com>
2023-01-20 22:37:39 -0600

    first commit

---
+++ main.js
@@ -0,0 +1,28 @@
+/**
+ * -------FORMULARIOS---------
+ * 
+ * --DENTRO DE LA ETIQUETA <form action="" <--AQUI VA LA DIRECCION A LA QUE SE DESEE ENVIAR EL FIRMULARIO  </form>
+ * 
+ * --Siempre en el <input></input> deve llevar la etiqueta ->
+ *  name = "nombre == for = "nombre" y esta debe ser igual a la de la etiqueta label 
+ * 
+ * 
+ * 
+ * 
+ * 
+ * 
+ * ---------TABLAS HTML----------
+ * 1) etiqueta <table></table>
+ * 2) Filas == <tr></tr> ---> tr == significa table Row
+ * 3) encabezado == Table Header == <th></th>
+ * 4)
+ * 
+ * 
+ * 
+ * 
+ * 
+ * 
+ * 
+ * 
+ * 
+ */
